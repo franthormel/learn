f6f2c8415218ff5979f238c324b149573131c72b
pendraic <caboyof@gmail.com>
2021-09-10 13:19:51 +0800

    Learning TypeScript

---
+++ language/ts/code/module-07/exercise01/main.js
@@ -0,0 +1,8 @@
+"use strict";
+exports.__esModule = true;
+var allGreetingFunctions = require("./greetings_module");
+var greetings_utilities_module_1 = require("./greetings-utilities_module");
+// Greetings_module
+allGreetingFunctions.returnGreeting('Hola!');
+// GreetingsWithLength_module
+greetings_utilities_module_1.returnGreeting('Ciao!');
---
+++ language/ts/code/module-07/exercise01/main.ts
@@ -0,0 +1,8 @@
+import * as allGreetingFunctions from "./greetings_module";
+import { returnGreeting as returnGreetingLength } from "./greetings-utilities_module";
+
+// Greetings_module
+allGreetingFunctions.returnGreeting('Hola!')
+
+// GreetingsWithLength_module
+returnGreetingLength('Ciao!')
