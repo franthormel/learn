5ec5c28608df6c095c2a26825625490411ad8642
neretzes <caboyof@gmail.com>
2021-12-31 09:02:34 +0800

    Delete chips component

--- framework/angular/material/src/app/components/chips/chips.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-chips',
-  templateUrl: './chips.component.html',
-  styleUrls: ['./chips.component.css'],
-})
-export class ChipsComponent {}
