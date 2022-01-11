abe1d174a2928fc62175e5b547ece68c8e7aba17
neretzes <caboyof@gmail.com>
2022-01-11 15:13:44 +0800

    Chapter 5 of Flutter Apprentice

---
+++ framework/flutter/flutter-apprentice/05-scrollable-widgets/fooderlich-challenge/lib/screens/explore_screen.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+import '../components/components.dart';
+import '../models/models.dart';
+import '../api/mock_fooderlich_service.dart';
+
+class ExploreScreen extends StatefulWidget {
+  ExploreScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ExploreScreen> createState() => _ExploreScreenState();
+}
+
+class _ExploreScreenState extends State<ExploreScreen> {
+  final mockService = MockFooderlichService();
+  late ScrollController _scrollController;
+
+  @override
+  void initState() {
+    _scrollController = ScrollController();
+    _scrollController.addListener(_scrollListener);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _scrollController.removeListener(_scrollListener);
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: mockService.getExploreData(),
+      builder: (context, AsyncSnapshot<ExploreData> snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          return ListView(
+            controller: _scrollController,
+            scrollDirection: Axis.vertical,
+            children: [
+              TodayRecipeListView(recipes: snapshot.data?.todayRecipes ?? []),
+              const SizedBox(height: 16),
+              FriendPostListView(friendPosts: snapshot.data?.friendPosts ?? []),
+            ],
+          );
+        } else {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+      },
+    );
+  }
+
+  void _scrollListener() {
+    if (_scrollController.offset >=
+            _scrollController.position.maxScrollExtent &&
+        !_scrollController.position.outOfRange) {
+      print('I am at the bottom!');
+    }
+
+    if (_scrollController.offset <=
+            _scrollController.position.minScrollExtent &&
+        !_scrollController.position.outOfRange) {
+      print('I am at the top!');
+    }
+  }
+}
--- framework/flutter/flutter-apprentice/README.md
+++ framework/flutter/flutter-apprentice/README.md
@@ -1,12 +1,26 @@
 # Flutter Apprentice 
-##  by Kevin D Moore, Michael Katz, Vincent Ngo
+##  by the raywenderlich Tutorial Team
 
 
-1. [Recipes](recipes/)
-   * Chapter 2: Hello, Flutter
-2. [Fooderlich](fooderlich/)
-   * Chapter 3: Basic Widgets
-   * Chapter 4: Understanding Widgets
+* ### [Chapter 2: Hello, Flutter](02-hello-flutter\recipes)
+
+* ### [Chapter 3: Basic Widgets](03-basic-widgets\fooderlich)
+  * Structure and navigation
+  * Displaying information
+  * Positioning widgets
+
+* ### [Chapter 4: Understanding Widgets](04-understanding-widgets\fooderlich)
+  * Widgets
+  * Widget rendering
+  * Flutter Inspector
+  * Types of widgets
+  * Widget lifecycle
+
+* ### [Chapter 5: Scrollable Widgets](05-scrollable-widgets\fooderlich) and [Challenges](05-scrollable-widgets\fooderlich-challenge) 
+  * How to use ListView.
+  * How to nest scroll views.
+  * How to leverage the power of GridView.
+
 
 ### Resources taken from [*the book's provided learning materials.*](https://github.com/raywenderlich/flta-materials)
 
