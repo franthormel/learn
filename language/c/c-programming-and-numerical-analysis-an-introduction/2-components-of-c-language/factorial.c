62db36a3ba25080ddc218824772ae8cd647e31bb
neretzes <caboyof@gmail.com>
2022-04-05 14:04:39 +0800

    Update factorial.c

--- language/c/c-programming-and-numerical-analysis-an-introduction/2-components-of-c-language/factorial.c
+++ language/c/c-programming-and-numerical-analysis-an-introduction/2-components-of-c-language/factorial.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#define N 15
 
 /*
 Write a function, int factorial(int n), which returns n! (the factorial of n, i.e.,
@@ -16,22 +17,23 @@ int factorial(int n)
 Using int factorial(int n) above, write a program to compute
 1 + 1/1! + 1/2! + ... + 1/11!.
 */
-// TODO: FIX
-float factorialSum(int n)
+double factorialSum(int n)
 {
-    
-    return 1.0 / (float)factorial(n - 1);
+    if (n == 0)
+        return 1;
+
+    return factorialSum(n - 1) + 1.0 / factorial(n);
 }
 
 int main()
 {
-    // for (int i = 0; i < 12; i++)
-    // {
-    //     printf("Factorial of %d =\t%d\n", i, factorial(i));
-    // }
-
-    for (int i = 0; i < 3; i++)
+    for (int i = 0; i < N; i++)
     {
-        printf("Factorial division sum of %d =\t%f\n", i, factorialSum(i));
+        printf("Factorial of %d =\t%d\n", i, factorial(i));
+    }
+
+    for (int i = 0; i < N; i++)
+    {
+        printf("Factorial division sum of %d =\t%.15lf\n", i, factorialSum(i));
     }
 }
