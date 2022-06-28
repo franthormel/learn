7bf6d8109ec969b3a3ad53b13d608c4561c6b9f4
Francis Anthony <caboyof@gmail.com>
2022-06-28 12:16:23 +0800

    Update Python QUICKSTART

--- language/python/QUICKSTART.py
+++ language/python/QUICKSTART.py
@@ -1,33 +1,34 @@
 # Python - Quickstart
 
-1. Variables
-```
+
+# 1. VARIABLES
+
 name = "Francis"
 age = 23
 isWorking = True
-```
 
-2. Control statements
-```
+
+# 2. CONTROL STATEMENTS
+
 if age < 23:
     print("Age is less than 23 years.")
 elif age > 23:
     print("Age is greater than 23 years.")
 else:
     print("Age is 23 years.")
-```
 
-3. Iteration
-```
+
+# 3. ITERATION
+
 for i in range(0, age):
     print("Hello, " + name + "!")
 
 while isWorking:
     print(f"Hello, {name}!")
-```
 
-4. Functions
-```
+
+# 4. FUNCTIONS
+
 def greet(name):
     print(f"Hello, {name}!")
 
@@ -39,10 +40,9 @@ def calculateSalary(isWorking):
         value = 100
 
     return value
-```
 
-5. Collections
-```
+
+# 5. COLLECTIONS
 nameList = ["Francis", "Anthony", "Carmel"]
 yearList = [2012, 2015, 2022]
 
@@ -50,10 +50,10 @@ materialsTuple = ("Textile", "Plastic", "Metal")
 
 subjectSet = set()
 yearSet = set([1, 2, 3, 4])
-```
 
-6. Classes
-```
+
+# 6. Classes
+
 class Point:
     def __init__(self, x, y):
         self.x = x
@@ -61,4 +61,3 @@ class Point:
 
     def printPoint(self):
         print(f"X: {self.x} Y: {self.y}")
-```
